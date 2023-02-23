c3660ba46ad38ed9c9c16d1b2a7a5fd6002e7617
badr42 <badr42@gmail.com>
2023-02-23 21:07:35 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,12 @@ sudo iptables -P OUTPUT ACCEPT
 sudo iptables -P FORWARD ACCEPT
 sudo iptables -F
 
+sudo apt-get install -y vim
+
 sudo su - ubuntu
 
+
+cd /home/ubuntu/
 # Install Node-RED
 #sudo bash
 curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered | bash -s -- --confirm-install --skip-pi --restart --confirm-root --no-init
@@ -26,7 +30,7 @@ export pass=`node -e "console.log(require('bcryptjs').hashSync(process.argv[1], 
 # Enable Node-RED security and set password
 if [ -n "$pass" ]; then
 #    sudo sed -i 's/^\(\s*\/\/\?\s*credentialSecret\s*:\s*\).*/\1"'$NR_PASS'";/' /root/.node-red/settings.js
-    sudo sed -i '/^\(\s*\/\/\?\s*adminAuth\s*:\s*\){/!b;n;c\    adminAuth: {\n        type: "credentials",\n        users: [{\n            username: "admin",\n            password: "'"$pass"'",\n            permissions: "*"\n        }]\n    },' /usr/lib/node_modules/node-red/settings.js
+    sudo sed -i '/^\(\s*\/\/\?\s*adminAuth\s*:\s*\){/!b;n;c\    adminAuth: {\n        type: "credentials",\n        users: [{\n            username: "admin",\n            password: "'"$pass"'",\n            permissions: "*"\n        }]\n    },' ~/.node-red/settings.js
 fi
 
 node-red-reload
