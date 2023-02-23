958d015ef8825c0b246361e598038c25588fe2e8
badr42 <badr42@gmail.com>
2023-02-23 21:07:29 +0300

    first commit

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+# Allow the firewall
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -F
+
+sudo su - ubuntu
+
+# Install Node-RED
+#sudo bash
+curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered | bash -s -- --confirm-install --skip-pi --restart --confirm-root --no-init
+
+
+if [ -z "$TP" ]; then
+    export TP=aGoodPassword
+fi
+
+#generate password 
+cd /usr/lib/node_modules/node-red/node_modules/
+export pass=`node -e "console.log(require('bcryptjs').hashSync(process.argv[1], 8));" $TP`
+
+
+# Enable Node-RED security and set password
+if [ -n "$pass" ]; then
+#    sudo sed -i 's/^\(\s*\/\/\?\s*credentialSecret\s*:\s*\).*/\1"'$NR_PASS'";/' /root/.node-red/settings.js
+    sudo sed -i '/^\(\s*\/\/\?\s*adminAuth\s*:\s*\){/!b;n;c\    adminAuth: {\n        type: "credentials",\n        users: [{\n            username: "admin",\n            password: "'"$pass"'",\n            permissions: "*"\n        }]\n    },' /usr/lib/node_modules/node-red/settings.js
+fi
+
+node-red-reload
+
+
+sudo systemctl enable nodered.service
+
+
+# Install Mosquitto
+sudo apt-get update
+sudo apt-get install -y mosquitto mosquitto-clients
+
+# Enable Mosquitto to start at boot
+sudo systemctl enable mosquitto
+
+
+# Show Mosquitto status
+sudo systemctl status mosquitto
+
+# Show Node-RED status
+sudo systemctl status nodered
